46bee511d0e0b813893590e2e60bd1e9b3247df7
Yann djoumessi <yanndjoumessi@gmail.com>
2023-04-06 11:35:31 +0100

    Setup of the project, routing has already been put in place and the design system is already defined

---
+++ insta-clone/src/app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./features/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'explore',
+    loadChildren: () =>
+      import('./features/explore/explore.module').then((m) => m.ExploreModule),
+  },
+  {
+    path: 'reels',
+    loadChildren: () =>
+      import('./features/reels/reels.module').then((m) => m.ReelsModule),
+  },
+  {
+    path: 'post',
+    loadChildren: () =>
+      import('./features/post/post.module').then((m) => m.PostModule),
+  },
+  {
+    path: 'direct',
+    loadChildren: () =>
+      import('./features/direct/direct.module').then((m) => m.DirectModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
